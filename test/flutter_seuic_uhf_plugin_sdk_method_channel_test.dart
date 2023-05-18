9bdcc5f36e088e08912d2da4df70828614c6fb7d
soullose <soullose@qq.com>
2023-05-18 10:05:58 +0800

    flutter analyze

--- test/flutter_seuic_uhf_plugin_sdk_method_channel_test.dart
+++ test/flutter_seuic_uhf_plugin_sdk_method_channel_test.dart
@@ -1,25 +1,14 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_seuic_uhf_plugin_sdk/flutter_seuic_uhf_plugin_sdk_method_channel.dart';
 
 void main() {
-  MethodChannelFlutterSeuicUhfPluginSdk platform =
-      MethodChannelFlutterSeuicUhfPluginSdk();
-  const MethodChannel channel = MethodChannel('flutter_seuic_uhf_plugin_sdk');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
-  });
+
 }
